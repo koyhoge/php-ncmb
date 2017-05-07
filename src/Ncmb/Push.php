cbab375244c177f51902652fcdd35ecb690d00ca
KOYAMA Tetsuji <koyama@hoge.org>
2017-05-07 09:45:23 +0000

    Import Push class.

---
+++ src/Ncmb/Push.php
@@ -0,0 +1,75 @@
+<?php
+namespace Ncmb;
+
+/**
+ * Push  - Handles sending push notifications with NCMB.
+ */
+class Push
+{
+    const VALID_OPTION_KEY = [
+        'deliveryTime',
+        'immediateDeliveryFlag',
+        'target',
+        'searchCondition',
+        'message',
+        'userSettingValue',
+        'deliveryExpirationDate',
+        'deliveryExpirationTime',
+        'action',
+        'title',
+        'dialog',
+        'badgeIncrementFlag',
+        'badgeSetting',
+        'sound',
+        'contentAvailable',
+        'richUrl',
+        'category',
+        'acl',
+    ];
+
+    /**
+     * Send push notification
+     * @param array $options
+     * @return string objectId of the registration for push
+     */
+    public static function send($options)
+    {
+        $data = static::encodeOptions($options);
+
+        $apiPath = 'push';
+        $apiOptions = [
+            'json' => $data,
+        ];
+        $response = ApiClient::post($apiPath, $apiOptions);
+
+        return $response['objectId'];
+    }
+
+    protected static function encodeOptions($options)
+    {
+        $deliveryTime = $immediateDeliveryFlag = null;
+        $data = [];
+
+        foreach ($options as $key => $val) {
+            if (!in_array($key, self::VALID_OPTION_KEY)) {
+                throw new Exception('Invalid option with Push::send');
+            }
+            if ($key === 'deliveryTime') {
+                $deliveryTime = $val;
+            }
+            if ($key === 'immediateDeliveryFlag') {
+                $immediateDeliveryFlag = $val;
+            }
+
+            if (is_object($val) && $val instanceof Encodable) {
+                $data[$key] = $val->encode();
+            } else {
+                $data[$key] = $val;
+            }
+        }
+        if ($deliveryTime === null && $immediateDeliveryFlag === null) {
+            throw new Exception('deliveryTime or immediateDeliveryFlag is required');
+        }
+        return $data;
+    }
+}
